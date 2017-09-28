c1db236d0b72b7d5f7b51e0ad760627be9fe9178
Austin Forsling <austin.forsling@gmail.com>
2017-09-28 15:52:24 -0600

    Finished GroupUp() method, finished Grouop class, added output() method.

--- cumulative chat/src/cumulative/chat/CumulativeChat.java
+++ cumulative chat/src/cumulative/chat/CumulativeChat.java
@@ -1,6 +1,8 @@
 package cumulative.chat;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Iterator;
 
@@ -13,7 +15,7 @@ public class CumulativeChat {
     HashSet<Student> studentTable = new HashSet<>();
     
     //Arraylist of Groups that the Group class can return to
-    ArrayList<Group> pairs;
+    ArrayList<Group> pairs = new ArrayList();
     
     //Iterates through allStudents and adds all the students to the HashSet
     public void GroupUp() {
@@ -29,9 +31,18 @@ public class CumulativeChat {
             if(hIterator.hasNext()){
                 s2 = hIterator.next();
                 pairs.add(new Group(s1,s2));
+                //System.out.println("made it past first pair");
             }
         }
+        //Sort the pairs by first name
+        Collections.sort(pairs, new Comparator<Group>(){
+            @Override
+            public int compare(Group g1, Group g2){
+                return g1.s1.Name().compareTo(g2.s1.Name());
+            }
+        });
         
+        output();
     }
     
     //Puts all the names of students on the ArrayList into a HashSet, to prevent duplicates.
@@ -39,6 +50,13 @@ public class CumulativeChat {
         Iterator<Student> iterator = allStudents.iterator();
         while(iterator.hasNext()){
             studentTable.add(iterator.next());
+        }
+    }
+    
+    private void output(){
+        Iterator<Group> gIter = pairs.iterator();
+        while(gIter.hasNext()){
+            System.out.println(gIter.next().s1.Name());
         }
     }
     
@@ -55,7 +73,5 @@ public class CumulativeChat {
         chat.add(new Student("Kenyon", "Brown", 92));
         chat.add(new Student("Nicholas", "Goldberg", 52));
         chat.GroupUp();
-    }
-
-    
+    }   
 }
--- cumulative chat/src/cumulative/chat/Group.java
+++ cumulative chat/src/cumulative/chat/Group.java
@@ -1,10 +1,18 @@
 package cumulative.chat;
 
+import java.util.ArrayList;
+
 public class Group {
-    //
+    //ArrayList that contains the pair of student objects
+    ArrayList<Student> pair = new ArrayList<>();
+    Student s1;
+    Student s2;
     
     //Groups pairs of students into one "chat room"
     public Group(Student first, Student second){
-        
+        s1 = first;
+        s2 = second;
+        pair.add(s1);
+        pair.add(s2);
     }
 }
