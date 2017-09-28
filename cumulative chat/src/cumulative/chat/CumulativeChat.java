ea983d1a869e2759f16e5d04a8cb06d891bc90ad
Austin Forsling <austin.forsling@gmail.com>
2017-09-28 12:16:01 -0600

    Initial commit, created student class

---
+++ cumulative chat/src/cumulative/chat/CumulativeChat.java
@@ -0,0 +1,15 @@
+package cumulative.chat;
+
+public class CumulativeChat {
+
+    public class Student{
+        
+        public void Student(){
+            
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
