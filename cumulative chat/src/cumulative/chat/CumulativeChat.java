88b8d737e148a363865ad72d5c5a74944e1ea810
Austin Forsling <austin.forsling@gmail.com>
2017-09-28 12:50:03 -0600

    added first name, last name strings; score int; and responses arraylist<string> to Student

--- cumulative chat/src/cumulative/chat/CumulativeChat.java
+++ cumulative chat/src/cumulative/chat/CumulativeChat.java
@@ -1,10 +1,20 @@
 package cumulative.chat;
 
+import java.util.ArrayList;
+
 public class CumulativeChat {
 
     public class Student{
         
-        public void Student(){
+        String fName = new String();
+        String lName = new String();
+        int score;
+        ArrayList<String> responses = new ArrayList<>();
+        
+        public void Student(String first, String last, int number){
+            fName = first;
+            lName = last;
+            score = number;
             
         }
     }
