992903a511c6dae1bb6dceed757d1d36080ebb8c
Trevor Marsh <tcmarsh@users.noreply.github.com>
2017-10-06 00:26:52 -0600

    Group specific issues
    You aren't using a unique collection for responses (which isn't a point loss), but are enforcing it another way - it would be easy enough to implement a solution to guarantee this, of course.
    Group also has unprotected access modifiers for it's instance variables (part of the same issue mentioned above).

--- cumulative chat/src/cumulative/chat/Group.java
+++ cumulative chat/src/cumulative/chat/Group.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 
 public class Group {
     //ArrayList that contains the pair of student objects
-    ArrayList<Student> pair = new ArrayList<>();
-    Student s1;
-    Student s2;
+    // "pair" is a variable that's not used for anything, and serves only to be a possible issue for later maintenance.
+    private ArrayList<Student> pair = new ArrayList<>();
+    private Student s1;
+    private Student s2;
     
     //Groups pairs of students into one "chat room"
     public Group(Student first, Student second){
@@ -18,6 +19,8 @@ public class Group {
     }
 
     private void addResponses() {
+        // You are reaching into an instance variable of another class.
+        // In any object oriented language this is an incredibly bad idea for multiple reasons - not least of which is that you can't verify inputs if anybody can alter variables any way they want to.
         s1.responses.add("Hello");
         s1.responses.add("How are you doing?");
         s1.responses.add("Doing well");
